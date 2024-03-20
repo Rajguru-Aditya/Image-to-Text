a6bd3cf442617859dab44972eb3cc132232856ad
Rajguru <adrajguru@deloitte.com>
2024-03-20 16:58:32 +0530

    Add a post request to get files from the frontend and extract text from it

--- flask_backend.py
+++ flask_backend.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from PIL import Image
 import pytesseract
 from flask_cors import CORS, cross_origin
@@ -11,6 +11,21 @@ CORS(app, support_credentials=True)
 @app.get('/text')
 def extractText():
     img = Image.open(image_path)
+    # img = "https://replicate.delivery/pbxt/Jj87qg6dTft3R5kFIzda2vorF3epnzwJpv96PsKcgkdZipLV/figure-65.png"
+
+    # Use pytesseract to do OCR on the image
+    text = pytesseract.image_to_string(img)
+
+    # cleaned_text = text.strip()
+
+    print("IMAGE to TEXT  --->", text)
+    return text
+
+@app.route('/gettext', methods=['POST'])
+def getfile():
+    file = request.files['file']
+    print("FILE --->", file)
+    img = Image.open(file)
     # img = "https://replicate.delivery/pbxt/Jj87qg6dTft3R5kFIzda2vorF3epnzwJpv96PsKcgkdZipLV/figure-65.png"
 
     # Use pytesseract to do OCR on the image
